3d5a3f8529d946ae5caa8d510a1c3ed0102d1212
bobonbon <sbb6987@naver.com>
2021-01-17 21:02:17 +0900

    fix cors

--- back/app.js
+++ back/app.js
@@ -29,14 +29,18 @@ if (process.env.NODE_ENV === 'production') {
     app.use(morgan('combined'));
     app.use(hpp());
     app.use(helmet());
+    app.use(cors({
+        origin: 'http://bobonbon.xyz',
+        credentials: true,
+    }));
 } else {
     app.use(morgan('dev'));
+    app.use(cors({
+        origin: true,
+        credentials: true,
+    }));
 }
 
-app.use(cors({
-    origin: [true, 'http://bobonbon.xyz'],
-    credentials: true,
-}));
 app.use('/', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
